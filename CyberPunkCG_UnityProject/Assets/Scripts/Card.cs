e480ce8b9fbcfbdeb38e037362ab5ea5b4289ef4
Enzo Ribas <enzorochaleitedinizribas@gmail.com>
2025-02-12 20:18:29 -0300

    first commit

---
+++ CyberPunkCG_UnityProject/Assets/Scripts/Card.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using System.Collections;
+[System.Serializable]
+
+public class Card : MonoBehaviour
+{
+    public int cardID;
+    public string cardName;
+    public string cardDescription;
+
+    public int cost;
+
+    
+}
